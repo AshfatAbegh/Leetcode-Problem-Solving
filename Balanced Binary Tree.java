e7f65b837ec45e6ce6304ad843828eaec21c3846
Ashfat <ashfat.rashid@polygontech.xyz>
2024-03-02 19:51:12 +0600

    Practicing Problem Solving

---
+++ Balanced Binary Tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+
+    public int height(TreeNode root){
+
+        if(root == null){
+            return 0;
+        }
+
+        int leftheight = height(root.left);
+        if(leftheight == -1){
+            return -1;
+        } 
+        int rightheight = height(root.right);
+        if(rightheight == -1){
+            return -1;
+        } 
+
+        if(Math.abs(leftheight-rightheight)>1){
+          return -1; 
+        }
+        return Math.max(leftheight,rightheight)+1;
+    }   
+}
