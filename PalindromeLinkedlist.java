fe49b874e608f9e19a1f69d5e42ada17bd2a5045
Ashfat Al Rashid <61910244+AshfatAbegh@users.noreply.github.com>
2024-09-23 14:01:03 +0600

    Add files via upload

---
+++ PalindromeLinkedlist.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+        if(head == null || head.next == null){
+            return true;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!= null && fast.next!= null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode firstHalf = head;
+        ListNode secondHalf = reverseList(slow);
+    
+        while(firstHalf.val != secondHalf.val){
+            return false;
+        }
+
+        firstHalf = firstHalf.next;
+        secondHalf = secondHalf.next;
+
+        return true;
+
+       }
+
+       private ListNode reverseList(ListNode head) {
+
+        ListNode previous = null;
+        ListNode current = head;
+
+        while (current != null) { 
+            ListNode temp = current.next;   
+            current.next = previous;
+            previous = current;
+            current = temp;
+        }
+
+        return previous;
+    }
+}
