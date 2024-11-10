0d22462ba2e630968f2b951252e230ff73b17ada
Ashfat Al Rashid <61910244+AshfatAbegh@users.noreply.github.com>
2024-11-10 18:20:07 +0600

    Add files via upload

---
+++ BinaryTreeInorderTraversal.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> list = new ArrayList();
+        inorderTraverse(root, list);
+        return list;
+    }
+
+    public void inorderTraverse(TreeNode root, List<Integer> list){
+
+        if(root == null){
+            return;
+        }
+
+        inorderTraverse(root.left, list);
+        list.add(root.val);
+        inorderTraverse(root.right, list);
+    }
+}
